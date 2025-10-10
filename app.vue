a2ac27c6b8a558daf1e11cffdc1121711fbc754f
crazydjon2 <roflea5@gmail.com>
2025-10-10 16:01:58 +0600

    Tg id print

--- app.vue
+++ app.vue
@@ -3,10 +3,16 @@
     <NuxtPage />
     <AppLoader />
     <LightHouseLoading />
+    <div class="z-100">
+      aaaa{{ tgUserData }}
+    </div>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 import AppLoader from './components/AppLoader.vue'
 import LightHouseLoading from './components/LightHouseLoading.vue'
+
+const tgUserData = Telegram.WebApp.initDataUnsafe.user
+console.log(tgUserData)
 </script>
